5586be2d83acdb6b3b61444b3f5bdfb1b13fc2f8
abhimanyu <abhimanyukrbeldari7@gmail.com>
2025-03-29 19:14:45 +0530

    Initial commit

---
+++ flipcart.js
@@ -0,0 +1,8 @@
+// let s1 = document.querySelectorAll("p");
+// s1 = addEventListener("Click", function(){
+//     s1.style.backgroundColor = "red";
+// });
+
+
+let s1 = document.querySelectorAll("logo");
+s1.style.color = "red;"
